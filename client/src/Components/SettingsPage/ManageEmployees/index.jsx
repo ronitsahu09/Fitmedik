7158d8789ecdf5a954c861fd9a127abe55201a3e
Vaasu Gambhir <55234743+vaasugambhir@users.noreply.github.com>
2023-01-05 21:42:07 +0530

    Fixed: loading page UI

--- client/src/Components/SettingsPage/ManageEmployees/index.jsx
+++ client/src/Components/SettingsPage/ManageEmployees/index.jsx
@@ -19,6 +19,7 @@ import { useNavigate } from "react-router-dom";
 import "../styles.css";
 import ConfirmDialog from "../../ConfirmDialog";
 import AddEmployeeDialog from "../AddEmployeeDialog";
+import LoadingPage from "../../LoadingPage";
 
 const ManageEmployees = () => {
   const [employees, setEmployees] = React.useState([
@@ -37,7 +38,7 @@ const ManageEmployees = () => {
   const delEmail = React.useRef(null);
 
   const GetEmployees = async () => {
-    setLoading(false);
+    // setLoading(false);
     setError(false);
     setErrorText("");
 
@@ -93,212 +94,226 @@ const ManageEmployees = () => {
   };
   return (
     <div>
-      <Grid container sx={{ mt: 4, mb: 4 }}>
-        <Grid item xs={1} />
-        <Grid container item xs={10} alignItems="center">
-          <Grid item xs={2} sx={{ mb: 4 }}>
-            <IconButton size={"large"} onClick={goBack}>
-              <KeyboardArrowLeft sx={{ fontSize: 64, color: "black" }} />
-            </IconButton>
-          </Grid>
-          <Grid item xs={10} sx={{ mb: 4 }}>
-            <Typography variant="h2" fontWeight="900">
-              Manage Employees
-            </Typography>
-          </Grid>
-          <Grid
-            container
-            item
-            xs={12}
-            style={{ border: "0.5px black", borderStyle: "solid none" }}
-            sx={{ p: 2 }}
-          >
-            <Grid item xs={2.5}>
-              <Typography variant="h6" fontWeight="800">
-                Name
-              </Typography>
-            </Grid>
-            <Grid item xs={2.5}>
-              <Typography variant="h6" fontWeight="800">
-                E-mail
-              </Typography>
-            </Grid>
-            <Grid item xs={2.5}>
-              <Typography variant="h6" fontWeight="800">
-                Status
-              </Typography>
-            </Grid>
-            <Grid item xs={2.5}>
-              <Typography variant="h6" fontWeight="800">
-                Department
-              </Typography>
-            </Grid>
-          </Grid>
-          {employees.map((val, index) => (
-            <Grid
-              container
-              item
-              xs={12}
-              key={index}
-              sx={{
-                p: 2,
-                backgroundColor: selectedEmployees.includes(index)
-                  ? "lightgrey"
-                  : "inherit",
-              }}
-              className={
-                index === employees.length - 1
-                  ? "settings-item settings-item-bottom"
-                  : "settings-item"
-              }
-              onClick={() => {
-                if (selectedEmployees.includes(index))
-                  setSelectedEmployees(
-                    selectedEmployees.filter((val) => val !== index)
-                  );
-                else setSelectedEmployees([...selectedEmployees, index]);
-              }}
-            >
-              <Grid item xs={2.5}>
-                <Typography variant="h6" fontWeight="100">
-                  {val.name}
-                </Typography>
-              </Grid>
-              <Grid item xs={2.5}>
-                <Typography variant="h6" fontWeight="100">
-                  {val.email}
-                </Typography>
-              </Grid>
-              <Grid item xs={2.5}>
-                <Typography variant="h6">
-                  {val.status === true ? (
-                    <Chip color="success" label="Active" variant="outlined" />
-                  ) : (
-                    <Chip color="error" label="Inactive" variant="outlined" />
-                  )}
-                </Typography>
-              </Grid>
-              <Grid item xs={2.5}>
-                <Typography variant="h6" fontWeight="100">
-                  {val.department}
-                </Typography>
-              </Grid>
-              <Grid item xs={2}>
-                <IconButton
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    delEmail.current = val.email;
-                    setConfirmOpen(true);
-                  }}
-                >
-                  <Delete />
+      {loading === true ? (
+        <LoadingPage />
+      ) : (
+        <div>
+          <Grid container sx={{ mt: 4, mb: 4 }}>
+            <Grid item xs={1} />
+            <Grid container item xs={10} alignItems="center">
+              <Grid item xs={2} sx={{ mb: 4 }}>
+                <IconButton size={"large"} onClick={goBack}>
+                  <KeyboardArrowLeft sx={{ fontSize: 64, color: "black" }} />
                 </IconButton>
               </Grid>
+              <Grid item xs={10} sx={{ mb: 4 }}>
+                <Typography variant="h2" fontWeight="900">
+                  Manage Employees
+                </Typography>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                style={{ border: "0.5px black", borderStyle: "solid none" }}
+                sx={{ p: 2 }}
+              >
+                <Grid item xs={2.5}>
+                  <Typography variant="h6" fontWeight="800">
+                    Name
+                  </Typography>
+                </Grid>
+                <Grid item xs={2.5}>
+                  <Typography variant="h6" fontWeight="800">
+                    E-mail
+                  </Typography>
+                </Grid>
+                <Grid item xs={2.5}>
+                  <Typography variant="h6" fontWeight="800">
+                    Status
+                  </Typography>
+                </Grid>
+                <Grid item xs={2.5}>
+                  <Typography variant="h6" fontWeight="800">
+                    Department
+                  </Typography>
+                </Grid>
+              </Grid>
+              {employees.map((val, index) => (
+                <Grid
+                  container
+                  item
+                  xs={12}
+                  key={index}
+                  sx={{
+                    p: 2,
+                    backgroundColor: selectedEmployees.includes(index)
+                      ? "lightgrey"
+                      : "inherit",
+                  }}
+                  className={
+                    index === employees.length - 1
+                      ? "settings-item settings-item-bottom"
+                      : "settings-item"
+                  }
+                  onClick={() => {
+                    if (selectedEmployees.includes(index))
+                      setSelectedEmployees(
+                        selectedEmployees.filter((val) => val !== index)
+                      );
+                    else setSelectedEmployees([...selectedEmployees, index]);
+                  }}
+                >
+                  <Grid item xs={2.5}>
+                    <Typography variant="h6" fontWeight="100">
+                      {val.name}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2.5}>
+                    <Typography variant="h6" fontWeight="100">
+                      {val.email}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2.5}>
+                    <Typography variant="h6">
+                      {val.status === true ? (
+                        <Chip
+                          color="success"
+                          label="Active"
+                          variant="outlined"
+                        />
+                      ) : (
+                        <Chip
+                          color="error"
+                          label="Inactive"
+                          variant="outlined"
+                        />
+                      )}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2.5}>
+                    <Typography variant="h6" fontWeight="100">
+                      {val.department}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <IconButton
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        delEmail.current = val.email;
+                        setConfirmOpen(true);
+                      }}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
-        <Grid item xs={1} />
-      </Grid>
+            <Grid item xs={1} />
+          </Grid>
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "fixed",
-          bottom: 50,
-          right: 50,
-          borderRadius: 99,
-        }}
-      >
-        <Button
-          variant="contained"
-          startIcon={<DoneAll />}
-          size="large"
-          disabled={selectedEmployees.length === employees.length}
-          onClick={() => {
-            let temp = [];
-            for (let i = 0; i < employees.length; i++) temp.push(i);
-            setSelectedEmployees(temp);
-          }}
-          sx={{ mr: 1 }}
-        >
-          Select All
-        </Button>
-        <Button
-          variant="contained"
-          startIcon={<ClearAll />}
-          size="large"
-          sx={{ mr: 1 }}
-          disabled={selectedEmployees.length === 0}
-          onClick={() => setSelectedEmployees([])}
-          color="error"
-        >
-          De-select All
-        </Button>
-        <Button
-          variant="contained"
-          color="success"
-          startIcon={<Add />}
-          size="large"
-          sx={{ mr: 1 }}
-          onClick={() => setAddOpen(true)}
-        >
-          Add employees
-        </Button>
-        <Badge badgeContent={selectedEmployees.length} color="error">
-          <Button
-            variant="contained"
-            startIcon={<Email />}
-            size="large"
-            disabled={selectedEmployees.length === 0}
-            onClick={() => setSendOpen(true)}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              position: "fixed",
+              bottom: 50,
+              right: 50,
+              borderRadius: 99,
+            }}
           >
-            Invite
-          </Button>
-        </Badge>
-      </div>
+            <Button
+              variant="contained"
+              startIcon={<DoneAll />}
+              size="large"
+              disabled={selectedEmployees.length === employees.length}
+              onClick={() => {
+                let temp = [];
+                for (let i = 0; i < employees.length; i++) temp.push(i);
+                setSelectedEmployees(temp);
+              }}
+              sx={{ mr: 1 }}
+            >
+              Select All
+            </Button>
+            <Button
+              variant="contained"
+              startIcon={<ClearAll />}
+              size="large"
+              sx={{ mr: 1 }}
+              disabled={selectedEmployees.length === 0}
+              onClick={() => setSelectedEmployees([])}
+              color="error"
+            >
+              De-select All
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              startIcon={<Add />}
+              size="large"
+              sx={{ mr: 1 }}
+              onClick={() => setAddOpen(true)}
+            >
+              Add employees
+            </Button>
+            <Badge badgeContent={selectedEmployees.length} color="error">
+              <Button
+                variant="contained"
+                startIcon={<Email />}
+                size="large"
+                disabled={selectedEmployees.length === 0}
+                onClick={() => setSendOpen(true)}
+              >
+                Invite
+              </Button>
+            </Badge>
+          </div>
 
-      {confirmOpen === true && (
-        <ConfirmDialog
-          open={confirmOpen}
-          header={"Employee will be removed"}
-          text={
-            "The employee will be removed and this action cannot be reversed."
-          }
-          onCancel={() => setConfirmOpen(false)}
-          onConfirm={() => {
-            setConfirmOpen(false);
-            DeleteEmployee(delEmail.current);
-          }}
-        />
-      )}
+          {confirmOpen === true && (
+            <ConfirmDialog
+              open={confirmOpen}
+              header={"Employee will be removed"}
+              text={
+                "The employee will be removed and this action cannot be reversed."
+              }
+              onCancel={() => setConfirmOpen(false)}
+              onConfirm={() => {
+                setConfirmOpen(false);
+                DeleteEmployee(delEmail.current);
+              }}
+            />
+          )}
 
-      {sendOpen === true && (
-        <ConfirmDialog
-          open={sendOpen}
-          header={"Send invitation E-mails?"}
-          text={
-            "An invitation e-mail will be sent to all employees, this action cannot be reversed."
-          }
-          onCancel={() => setSendOpen(false)}
-          onConfirm={() => {
-            setSendOpen(false);
-            SendInvite();
-          }}
-        />
-      )}
+          {sendOpen === true && (
+            <ConfirmDialog
+              open={sendOpen}
+              header={"Send invitation E-mails?"}
+              text={
+                "An invitation e-mail will be sent to all employees, this action cannot be reversed."
+              }
+              onCancel={() => setSendOpen(false)}
+              onConfirm={() => {
+                setSendOpen(false);
+                SendInvite();
+              }}
+            />
+          )}
 
-      {addOpen === true && (
-        <AddEmployeeDialog
-          open={addOpen}
-          onCancel={() => setAddOpen(false)}
-          onConfirm={(name, email, department) => {
-            setAddOpen(false);
-            AddEmployee(name, email, department);
-          }}
-        />
+          {addOpen === true && (
+            <AddEmployeeDialog
+              open={addOpen}
+              onCancel={() => setAddOpen(false)}
+              onConfirm={(name, email, department) => {
+                setAddOpen(false);
+                AddEmployee(name, email, department);
+              }}
+            />
+          )}
+        </div>
       )}
     </div>
   );
